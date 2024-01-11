505f9d0fffc4daea258eb016d3256c0582abfeb4
Ramith Jayasinghe <3402882+ramith@users.noreply.github.com>
2024-01-11 10:21:40 +0530

    generated the project

---
+++ TheEQ/TheEQApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheEQApp.swift
+//  TheEQ
+//
+//  Created by Ramith Jayasinghe on 2024-01-11.
+//
+
+import SwiftUI
+
+@main
+struct TheEQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
